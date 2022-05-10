7ed84ac17d02a5c7d624d01a92848a974bbd86e6
huangdi <1013697816@qq.com>
2022-05-10 17:06:36 +0800

    分析页

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { defineStore } from "pinia";
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export const userStore = defineStore("user", {
+  state: () => ({
+    username: "",
+    password: "",
+    lesson1: 116,
+    lesson2: 122,
+    lesson3: 98,
+    otherUsers: [
+      {
+        username: "user2",
+        lesson1: 90,
+        lesson2: 131,
+        lesson3: 110,
+      },
+      {
+        username: "user3",
+        lesson1: 89,
+        lesson2: 92,
+        lesson3: 145,
+      },
+      {
+        username: "user4",
+        lesson1: 128,
+        lesson2: 114,
+        lesson3: 99,
+      },
+    ],
+  }),
+});
